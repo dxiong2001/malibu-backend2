7d33594e8ce3e57deaadaf9cfa0e645d0da187fc
David Xiong <xiongdsat@gmail.com>
2022-07-24 10:15:12 -0700

    updated backend structure and database  models

--- api/models.py
+++ api/models.py
@@ -6,22 +6,22 @@ class Entity(models.Model):
     name = models.CharField(max_length=200)
     userName = models.CharField(max_length=200)
     screenName = models.CharField(max_length = 200)
-    profileImg = models.CharField()
+    profileImg = models.TextField()
 
 class Quote(models.Model):
     author = models.ForeignKey(Entity, on_delete=models.CASCADE)
-    text = models.CharField()
+    text = models.TextField()
 
 class Section(models.Model):
-    text = models.CharField()
+    text = models.TextField()
     quotes = models.ManyToManyField(Quote)
 
 class Tweet(models.Model):
     _id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField()
-    subtitle = models.CharField()
-    time = models.CharField()
-    publisher = models.ForeignKey(Entity, on_delete=models.CASCADE)
-    author = models.ForeignKey(Entity, on_delete=models.CASCADE)
-    image = models.CharField()
+    title = models.TextField()
+    subtitle = models.TextField()
+    time = models.TextField()
+    publisher = models.ForeignKey(Entity, on_delete=models.CASCADE,related_name='publisher')
+    author = models.ForeignKey(Entity, on_delete=models.CASCADE,related_name='author')
+    image = models.TextField()
     sections = models.ManyToManyField(Section)
--- cfehome/settings.py
+++ cfehome/settings.py
@@ -81,10 +81,20 @@ WSGI_APPLICATION = 'cfehome.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'Client': {
+            'host': os.getenv('MONGO_KEY', 'Optional default value'),
+            'name': 'Tweets',
+            'authMechanism': 'SCRAM-SHA-1',
+        },
     }
 }
 
