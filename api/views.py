0b7ce51ef45740518ee226ee0d7886e8764f6ac3
David Xiong <xiongdsat@gmail.com>
2022-08-07 00:00:12 -0700

    moved non-api related functions to new helper.py to reduce confusion

--- api/views.py
+++ api/views.py
@@ -1,11 +1,11 @@
 import json
 from django.http import JsonResponse
-from .parser import *
-from .extraction import Summarizer, textrank
+from .helper import *
 from django.views.decorators.csrf import csrf_exempt
 import urllib
 import nltk.tokenize.texttiling as tt
 from rest_framework.decorators import api_view
+
 
 from api.models import Tweet
 from api.serializers import TweetSerializer
@@ -30,82 +30,6 @@ def tweetsApi(request, *args, **kwargs):
                 array_tweets.append(t['tweet'])
         return JsonResponse(array_tweets, safe=False)
     return JsonResponse("Invalid request", safe=False)
-        
-def getTweet2(url, article_url):
-
-    texttiler = tt.TextTilingTokenizer(w=30, k=40)
-    summary = Summarizer(texttiler)
-
-    page_content = get_html(url)
-    body_content = getArticleBody(page_content)
-    article_sections, article_p, article_subtitles = getArticleBodySections(body_content)
-
-    if(len(article_sections)==1):
-        article_body_text = "\n\n".join(article_p)
-        article_sections = summary.texttile(article_body_text)
-    
-    article_people = get_names(article_sections)
-    attributed_quotes = get_quotes(article_sections, article_people)
-    
-
-
-def getTweet1(url, article_url):
-    page_content = get_html(url)
-    processed_text = getArticleTextSections(page_content)
-    article_section = processArticleSections(processed_text)
-    # print("--- %s seconds ---" % (time.time() - start_time))
-    # start_time = time.time()
-
-
-    texttiler = tt.TextTilingTokenizer(w=30, k=40)
-    summary = Summarizer(texttiler)
-    sections, summarized_sections = summary.generate(article_section, 1)
-    #sections, summarized_sections = q.enqueue(summary.generate, article_section)
-    # print("--- %s seconds ---" % (time.time() - start_time))
-    # start_time = time.time()
-
-    quotes = getQuotes(processed_text)
-
-    title, subtitle, author, date, image = getArticleInfo(page_content)
-    publisher = getArticlePublisher(page_content)
-    people, people_extended = getNamedEntities(processed_text)
-    attributed_quotes = attribute_quote(people_extended, quotes)
-
-    authorEntity = createSingularEntity(author)
-    publisherEntity = createSingularEntity(publisher)
-
-    QuotesList = []
-    for a in attributed_quotes:
-        QuotesList.append(createQuote(a[0],a[1]))
-
-    SectionList = quoteToSection(sections, summarized_sections, QuotesList)
-    SectionList = processFirstSection(SectionList)
-    #print(generateEntitiesList(people))
-    
-    #print(url)
-    #print(title, author,date,image)
-    #print(attributed_quotes)
-
-
-
-
-    Tweet_ = {'_id': "1232", 'author': authorEntity, 'time': date, 'title': title, 'subtitle': subtitle, 'image': image, 'publisher': publisherEntity, 'sections': SectionList}
-    # data['headers'] = dict(request.headers)
-    # data['content_type'] = request.content_type
-    
-    json_tweet = json.dumps(Tweet_)
-    serializer = TweetSerializer(data={'url': article_url, 'tweet': json_tweet})
-    db_store = {'url': article_url, 'tweet': json_tweet}
-    # print("--- %s seconds ---" % (time.time() - start_time))
-    
-    
-    if serializer.is_valid():
-        serializer.save()
-        print("valid")
-    else:
-        print("not valid or error")
-    
-    return Tweet_
 
 
 
