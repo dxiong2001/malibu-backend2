298597cd89de2a3b40c004b5fb60a06371a36215
David Xiong <xiongdsat@gmail.com>
2022-09-06 10:09:07 -0700

    moved summarizer instantiation into function

--- api/abstraction.py
+++ api/abstraction.py
@@ -1,9 +1,10 @@
 from transformers import pipeline
 
 #classifier = pipeline("summarization", model="t5-base")
-summarizer = pipeline("summarization", model="t5-small", tokenizer="t5-small", framework="tf")
+
 
 def abs_summarization(article_sections):
+    summarizer = pipeline("summarization", model="t5-small", tokenizer="t5-small", framework="tf")
     summ_sections = []
     for text in article_sections:
         summ_sections.append(summarizer(text, max_length = 100, min_length = 10, do_sample=False)[0]['summary_text'])
