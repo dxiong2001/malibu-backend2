dbfc39bbd709fd593ef43ce95a493d885f0bbaa5
David Xiong <xiongdsat@gmail.com>
2022-09-06 06:44:26 -0700

    modified abstraction function

--- api/abstraction.py
+++ api/abstraction.py
@@ -5,6 +5,6 @@ classifier = pipeline("summarization")
 
 def abs_summarization(article_sections):
     summ_sections = []
-    for text in article_sections:
-        summ_sections.append(classifier(text, max_length = 100, min_length = 10, do_sample=False)[0]['summary_text'])
+    #for text in article_sections:
+        #summ_sections.append(classifier(text, max_length = 100, min_length = 10, do_sample=False)[0]['summary_text'])
     return summ_sections
