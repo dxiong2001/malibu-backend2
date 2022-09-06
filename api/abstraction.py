86e76b269f69b494bae7b3617137f2ff3ea49cde
David Xiong <xiongdsat@gmail.com>
2022-09-06 10:02:29 -0700

    changed abstractive summarization model from t5-base to t5-small

--- api/abstraction.py
+++ api/abstraction.py
@@ -1,10 +1,10 @@
 from transformers import pipeline
 
 #classifier = pipeline("summarization", model="t5-base")
-classifier = pipeline("summarization")
+summarizer = pipeline("summarization", model="t5-small", tokenizer="t5-small", framework="tf")
 
 def abs_summarization(article_sections):
     summ_sections = []
-    #for text in article_sections:
-        #summ_sections.append(classifier(text, max_length = 100, min_length = 10, do_sample=False)[0]['summary_text'])
+    for text in article_sections:
+        summ_sections.append(summarizer(text, max_length = 100, min_length = 10, do_sample=False)[0]['summary_text'])
     return summ_sections
